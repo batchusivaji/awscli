315b8538e5637ae54aeb7ccea19a770ed09dc165
Your Name <you@example.com>
2023-03-21 18:02:01 +0530

    added activity

--- activity1.sh
+++ activity1.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+tag_name=$1
+tag_value=$2
+if [[ -z $"tag_name" ]]; then
+    tag_name= "Env"
+
 instanceIds=$(aws ec2 describe-instances --filter "Name=tag:Env,Values=QA" "Name=instance-state-name,Values=running" --query Reservations[].Instances[].InstanceId --output text)
-if [[ -n $"instanceIds"]]; then
+if [[ -n $"instanceIds" ]]; then
 echo "the instace ids which will be shutdown are {instancesIds}"
 aws ec2 stop-instances --instance-ids ${instanceIds}
 else
-echo "no instanes found mathins criteria Env=QA"
+echo "no instanes found with matching criteria Env = QA"
 fi
